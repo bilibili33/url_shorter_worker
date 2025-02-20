365429be90187cae07ca11b4b2fa30a84e67672c
BiliEne <29067324+bilibili33@users.noreply.github.com>
2025-02-21 03:22:44 +0900

    Create worker.js
    删的只剩个首页了，改了一下404让自动回首页

---
+++ worker.js
@@ -0,0 +1,56 @@
+const config = {
+  theme:""//Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
+  }
+  
+  const html404 = `<!DOCTYPE html>
+  <body>
+    <h1>404 Not Found.</h1>
+    <p>The url you visit is not found.</p>
+    <a href="https://github.com/xyTom/Url-Shorten-Worker/" target="_self">Fork me on GitHub</a>
+  </body>
+  <script>
+    let goback=window.location.protocol+"//"+window.location.host;
+    setInterval('window.location.replace(goback)', 5000)
+  </script>`
+  
+  async function handleRequest(request) {  
+    const requestURL = new URL(request.url)
+    const path = requestURL.pathname.split("/")[1]
+    const params = requestURL.search;
+  
+    console.log(path)
+    if(!path){
+  
+      const html= await fetch("https://xytom.github.io/Url-Shorten-Worker/"+config.theme+"/index.html")
+      
+      return new Response(await html.text(), {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    })
+    }
+    console.log("path: "+path)
+
+    const value = await shorter.get(path);
+    let location ;
+  
+    if(params) {
+      location = value + params
+    } else {
+      location = value
+    }
+    console.log(value)
+
+    // If request not in kv, return 404
+    return new Response(html404, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+      status: 404
+    })
+  }
+  
+  
+  addEventListener("fetch", async event => {
+    event.respondWith(handleRequest(event.request))
+  })
